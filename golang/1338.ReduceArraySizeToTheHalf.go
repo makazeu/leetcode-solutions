b81a1e6542f393671988a47d8a7018bd2998954f
Makazeu <makazeu@gmail.com>
2020-02-09 16:12:54 +0900

    Weekly Contest 174

---
+++ golang/1338.ReduceArraySizeToTheHalf.go
@@ -0,0 +1,22 @@
+func minSetSize(arr []int) int {
+    hash := make(map[int]int)
+    for _, x := range arr {
+        hash[x]++
+    }
+    
+    var nums []int
+    for _, v := range hash {
+        nums = append(nums, v)
+    }
+    
+    sort.Ints(nums)
+    l := len(nums)
+    now := 0
+    for i := l-1; i >= 0; i-- {
+        now += nums[i]
+        if now >= len(arr)/2 {
+            return l-i
+        }
+    }
+    return -1
+}
